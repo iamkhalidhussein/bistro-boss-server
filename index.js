023b0e0e4c8f0695bcd13eb92afda87b31b2199a
khalid09dev <mdkhalidhossen@hotmail.com>
2024-05-26 11:43:38 +0600

    ...

--- index.js
+++ index.js
@@ -128,6 +128,20 @@ async function run() {
             res.send(result);
         })
 
+        app.post('/menu', verifyToken, verifyAdmin, async(req, res) => {
+            const item = req.body;
+            console.log(item)
+            const result = await menuCollection.insertOne(item);
+            res.send(result);
+        })
+
+        app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await menuCollection.deleteOne(query);
+            res.send(result);
+        })
+
         app.get('/reviews', async(req, res) => {
             const result = await reviewCollection.find().toArray();
             res.send(result);
